89a385c5efd5140e6b6e8687fe27cd174481849f
dreamermx123 <dzargo1@yandex.ru>
2024-11-21 15:54:50 +0300

    horizonral scroll

---
+++ server/public/horizontal_scroll/horizontal_scroll.js
@@ -0,0 +1,9 @@
+// const horizontalScroll = document.querySelector('.horizontal-scroll');
+
+// // Обработчик события scroll
+// window.addEventListener('scroll', () => {
+//     const scrollPosition = window.pageYOffset; // Текущая вертикальная прокрутка
+//     horizontalScroll.style.transform = `translateX(-${scrollPosition}px)`; // Перемещаем блок горизонтально
+// });
+
+
